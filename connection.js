a5d5d51e5f7419c4926e0c425b2c39ab60595cd6
Raushan Kumar <raushankumar2909@gmail.com>
2024-04-29 18:51:05 +0530

    Update .gitignore to include details.txt and modify connection.js to use environment variable for MongoDB URI

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://raushan:raushan2504@cluster0.hwvaxa9.mongodb.net/clipboard?retryWrites=true&w=majority&appName=Cluster0`;
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017/clipper`;
 
 mongoose.connect(url).then(()=>{console.log('Connected to database')}).catch((err)=>{console.log('Error in connecting to database',err)});
