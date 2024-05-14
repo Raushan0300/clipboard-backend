3c8e5fa2a33212d75f0aa56ae47a56cf49914195
Raushan Kumar <raushankumar2909@gmail.com>
2024-05-14 20:05:32 +0530

    Refactor connection.js to use 'clipnet' as the default MongoDB URI

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI || `mongodb://localhost:27017/clipper`;
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017/clipnet`;
 
 mongoose.connect(url).then(()=>{console.log('Connected to database')}).catch((err)=>{console.log('Error in connecting to database',err)});
